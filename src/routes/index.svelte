9e8bcb5dee1a085a439f7983bde712ada8a3d6d9
Nahuel Chaves <nahuel.chaves@gmail.com>
2020-04-17 23:59:35 -0300

    removed extra stuff

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -21,37 +21,7 @@
         cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua
         ad ad non deserunt sunt.
       </p>
-      <div
-        class="mt-5 sm:max-w-lg sm:mx-auto sm:text-center lg:text-left lg:mx-0">
-        <p class="text-base font-medium text-gray-900">
-          Sign up to get notified when it’s ready.
-        </p>
-        <form action="#" method="POST" class="mt-3 sm:flex">
-          <input
-            aria-label="Email"
-            class="appearance-none block w-full px-3 py-3 border border-gray-300
-            text-base leading-6 rounded-md placeholder-gray-500 shadow-sm
-            focus:outline-none focus:placeholder-gray-400 focus:shadow-outline
-            focus:border-blue-300 transition duration-150 ease-in-out sm:flex-1"
-            placeholder="Enter your email" />
-          <button
-            type="submit"
-            class="mt-3 w-full px-6 py-3 border border-transparent text-base
-            leading-6 font-medium rounded-md text-white bg-gray-800 shadow-sm
-            hover:bg-gray-700 focus:outline-none focus:shadow-outline
-            active:bg-gray-900 transition duration-150 ease-in-out sm:mt-0
-            sm:ml-3 sm:flex-shrink-0 sm:inline-flex sm:items-center sm:w-auto">
-            Notify me
-          </button>
-        </form>
-        <p class="mt-3 text-sm leading-5 text-gray-500">
-          We care about the protection of your data. Read our
-          <a href="#" class="font-medium text-gray-900 underline">
-            Privacy Policy
-          </a>
-          .
-        </p>
-      </div>
+     
     </div>
     <div
       class="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0
@@ -100,21 +70,6 @@
             class="w-full"
             src="https://images.unsplash.com/photo-1556740758-90de374c12ad?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
             alt="Woman making a sale" />
-          <div
-            class="absolute inset-0 w-full h-full flex items-center
-            justify-center">
-            <svg
-              class="h-20 w-20 text-indigo-500"
-              fill="currentColor"
-              viewBox="0 0 84 84">
-              <circle opacity="0.9" cx="42" cy="42" r="42" fill="white" />
-              <path
-                d="M55.5039 40.3359L37.1094 28.0729C35.7803 27.1869 34 28.1396
-                34 29.737V54.263C34 55.8604 35.7803 56.8131 37.1094
-                55.9271L55.5038 43.6641C56.6913 42.8725 56.6913 41.1275 55.5039
-                40.3359Z" />
-            </svg>
-          </div>
         </button>
       </div>
     </div>
