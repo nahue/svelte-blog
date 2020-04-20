3ff3e3533fde3a95760d3198e3c3bd3ea200abce
Nahuel Chaves <nahuel.chaves@gmail.com>
2020-04-20 18:50:25 -0300

    landing page style

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,36 +1,50 @@
 <svelte:head>
-  <title>Nahuel Chaves</title>
+    <title>Nahuel Chaves</title>
 </svelte:head>
 
 <!--<div
   class="mt-8 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-20 xl:mt-24">-->
-<div class="m-auto">
-  <div class="mx-auto max-w-screen-xl px-4 sm:px-6">
-    <div class="lg:grid lg:grid-cols-12 lg:gap-8">
-      <div
-        class="sm:text-center md:max-w-2xl lg:col-span-6 lg:text-left">
-        <h2
-          class="mt-1 text-4xl tracking-tight leading-10 font-extrabold
-          text-gray-900 sm:leading-none sm:text-6xl lg:text-5xl xl:text-6xl">
-          Nahuel Chaves
-          <br class="hidden md:inline" />
-          <span class="bg-gray-900 text-white inline-block px-2 py-1">
-            Software
-          </span>
-          <span class="bg-gray-900 text-white inline-block px-2 py-1">
-            Engineer
-          </span>
-        </h2>
+<div class="m-auto select-none">
+    <div class="mx-auto max-w-screen-xl px-4 sm:px-6">
+        <div class="lg:grid lg:grid-cols-12 lg:gap-8">
+            <div class="sm:text-center md:max-w-2xl lg:col-span-6 lg:text-left">
 
-      </div>
-      <div
-        class="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none
-        lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
-        <p class="text-base text-gray-500 sm:text-xl lg:text-lg xl:text-xl">
-          I create from lambdas all the way to full blown web applications,
-          always using well-tested cutting-edge technologies.
-        </p>
-      </div>
+                <div
+                    class="mt-1 text-4xl tracking-tight leading-10
+                    font-extrabold text-gray-900 sm:leading-none sm:text-6xl
+                    lg:text-5xl xl:text-6xl">
+                    <div class="flex flex-col">
+                        <div class="flex items-center justify-end mb-1">
+                            <img
+                                class="inline-block h-16 w-16 rounded-full mr-4"
+                                src="/avatar.jpeg"
+                                alt="" />
+                            <span class="inline-block align-bottom">
+                                Nahuel Chaves
+                            </span>
+                        </div>
+                        <div class="flex items-center justify-end">
+                            <span class="bg-gray-700 text-white inline-block px-2 py-1">
+                                Software
+                            </span>
+                            <span class="bg-gray-700 text-white inline-block px-2 py-1">
+                                Engineer
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div
+                class="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0
+                lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+                <p
+                    class="text-base text-gray-500 sm:text-xl lg:text-lg
+                    xl:text-xl">
+                    I create from lambdas all the way to full blown web
+                    applications, always using well-tested cutting-edge
+                    technologies.
+                </p>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
