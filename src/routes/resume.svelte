35ea4848b22e3337ac4ced7c6c28e7169c865bc1
Nahuel Chaves <nahuel.chaves@gmail.com>
2020-05-14 00:34:31 -0300

    upgrade deps and add resume

---
+++ src/routes/resume.svelte
@@ -0,0 +1,83 @@
+<section class="w-full mx-auto px-0 lg:w-1/2 sm:my-auto sm:w-1/2 xs:px-10">
+    <div>
+        <span
+            class="text-4xl font-extrabold text-gray-900 sm:leading-none
+            sm:text-6xl lg:text-5xl xl:text-4xl align-bottom text-gray-800">
+            Nahuel Chaves
+        </span>
+        <p class="max-w-2xl text-sm leading-5 text-gray-800">
+            A brief summary of my professional life.
+        </p>
+
+        <p class="text-sm leading-5 text-gray-500 mt-5">
+            For the last 6 years i worked mainly on remote positions, so i can
+            say i'm able to balance my personal and work life. Before that i
+            worked for the local goverment in the IT department. I started
+            repairing computers as a teenager, and after a few years i reached a
+            Director position, so i ended up in charge of an entire Datacenter
+            keeping Personal and Financial info, as well as Services for the
+            other public workers such as Websites, Email, File Servers, User
+            Directory, CRM and Project Management platforms)...
+        </p>
+        <p class="text-sm leading-5 text-gray-500 mt-5">
+            But i feel that's not as important as my "newly" remote life, so
+            let's get to it.
+        </p>
+    </div>
+    <div class="mt-5 border-t border-gray-200 pt-5">
+        <dl>
+            <div class=" sm:grid sm:grid-cols-3 sm:gap-4">
+                <dt class="text-sm leading-5 font-medium text-gray-500">
+                    Seats.io (Oct 2018 - Apr 2020)
+                    <span class="block font-light">FullStack</span>
+                </dt>
+                <dd
+                    class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0
+                    sm:col-span-2">
+                    Saas for building Seating Charts, its main focus is
+                    Ticketing Platforms. I worked both in the Java based core
+                    and Typescript based frontend, as well as all the
+                    integrations in a plethora of stacks
+                    <strong>
+                        (Java, JS, Node, PHP, Ruby, Python, React, Angular, IOS,
+                        Android)
+                    </strong>
+                </dd>
+            </div>
+            <div
+                class="mt-8 sm:grid sm:mt-5 sm:grid-cols-3 sm:gap-4 sm:border-t
+                sm:border-gray-200 sm:pt-5">
+                <dt class="text-sm leading-5 font-medium text-gray-500">
+                    Zentricx (Dec 2014 - Oct 2018)
+                    <span class="block font-light">DevOps - FullStack</span>
+                </dt>
+                <dd
+                    class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0
+                    sm:col-span-2">
+                    Managed a multicloud environment (Azure and AWS). Also i was
+                    involved in ASP.NET development for one of their clients.
+                </dd>
+
+            </div>
+            <div
+                class="mt-8 sm:grid sm:mt-5 sm:grid-cols-3 sm:gap-4 sm:border-t
+                sm:border-gray-200 sm:pt-5">
+                <dt class="text-sm leading-5 font-medium text-gray-500">
+                    Valley Works (Dec 2014 - Oct 2018)
+                    <span class="block font-light">DevOps - FullStack</span>
+                </dt>
+                <dd
+                    class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0
+                    sm:col-span-2">
+                    I was involved in several projects, mainly to developing
+                    custom apps using Python and Javascript, using Frameworks
+                    like
+                    <strong>Django</strong>
+                    and
+                    <strong>React</strong>
+                    .
+                </dd>
+            </div>
+        </dl>
+    </div>
+</section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const { colors, screens } = require('tailwindcss/defaultTheme')
 
 module.exports = {
+    purge: [ "src/**/*.svelte" ],
     theme: {
         screens: {
             'xs': '375px',
